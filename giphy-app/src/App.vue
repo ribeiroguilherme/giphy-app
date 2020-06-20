0d77b62cb0c2944df2868864110a3c26c121cef6
Guilherme Ribeiro <guilhermemrr@gmail.com>
2020-06-20 14:32:31 +0200

    feat: skeleton

---
+++ giphy-app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <tag />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Tag from './components/Tag.vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+@Component({
+  components: {
+    HelloWorld,
+    Tag,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ giphy-app/src/components/Tag.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="tag">
+    Hello!
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'tag',
+});
+</script>
+
+<style scoped>
+</style>
